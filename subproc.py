45eb304bb973505d7bf8edda09a69c3381a278b9
Sergey <xxsmotur@gmail.com>
2019-01-20 18:17:30 +0500

    remove usless methods

--- subproc.py
+++ subproc.py
@@ -25,18 +25,6 @@ class Command(object):
         return "<Command {!r}>".format(self.cmd)
 
     @property
-    def _default_popen_kwargs(self):
-        return {
-            "env": os.environ.copy(),
-            "stdin": subprocess.PIPE,
-            "stdout": subprocess.PIPE,
-            "stderr": subprocess.PIPE,
-            "shell": True,
-            "universal_newlines": True,
-            "bufsize": 0,
-        }
-
-    @property
     def _default_pexpect_kwargs(self):
         encoding = "utf-8"
         if sys.platform == "win32":
@@ -78,81 +66,24 @@ class Command(object):
         return result
 
     @property
-    def out(self):
-        """Std/out output (cached)"""
-        if self.__out is not None:
-            return self.__out
-
-        if self._uses_subprocess:
-            self.__out = self.std_out.read()
-        else:
-            self.__out = self._pexpect_out
-
-        return self.__out
-
-    @property
-    def std_err(self):
-        return self.subprocess.stderr
-
-    @property
-    def err(self):
-        """Std/err output (cached)"""
-        if self.__err is not None:
-            return self.__err
-
-        if self._uses_subprocess:
-            self.__err = self.std_err.read()
-            return self.__err
-        else:
-            return self._pexpect_out
-
-    @property
     def pid(self):
-        """The process' PID."""
-        # Support for pexpect's functionality.
         if hasattr(self.subprocess, "proc"):
             return self.subprocess.proc.pid
-        # Standard subprocess method.
         return self.subprocess.pid
 
     @property
     def return_code(self):
-        # Support for pexpect's functionality.
         if self._uses_pexpect:
             return self.subprocess.exitstatus
-        # Standard subprocess method.
         return self.subprocess.returncode
 
     @property
     def std_in(self):
         return self.subprocess.stdin
 
-    def run(self, block=True, binary=False, cwd=None, env=None):
-        """Runs the given command, with or without pexpect functionality enabled."""
+    def run(self, block=True, binary=False, cwd=None, env=None, args=None):
         self.blocking = block
-
-        # Use subprocess.
-        if self.blocking:
-            popen_kwargs = self._default_popen_kwargs.copy()
-            popen_kwargs["universal_newlines"] = not binary
-            if cwd:
-                popen_kwargs["cwd"] = cwd
-            if env:
-                popen_kwargs["env"].update(env)
-            s = subprocess.Popen(self._popen_args, **popen_kwargs)
-        # Otherwise, use pexpect.
-        else:
-            pexpect_kwargs = self._default_pexpect_kwargs.copy()
-            if binary:
-                pexpect_kwargs["encoding"] = None
-            if cwd:
-                pexpect_kwargs["cwd"] = cwd
-            if env:
-                pexpect_kwargs["env"].update(env)
-            # Enable Python subprocesses to work with expect functionality.
-            pexpect_kwargs["env"]["PYTHONUNBUFFERED"] = "1"
-            s = PopenSpawn(self._popen_args, **pexpect_kwargs)
-        self.subprocess = s
+        self.subprocess = subprocess.Popen(args)
         self.was_run = True
 
     def pipe(self, command, timeout=None, cwd=None):
@@ -175,47 +106,3 @@ class Command(object):
             c.subprocess.sendeof()
         c.block()
         return c
-
-
-def _expand_args(command):
-    """Parses command strings and returns a Popen-ready list."""
-
-    # Prepare arguments.
-    if isinstance(command, STR_TYPES):
-        if sys.version_info[0] == 2:
-            splitter = shlex.shlex(command.encode("utf-8"))
-        elif sys.version_info[0] == 3:
-            splitter = shlex.shlex(command)
-        else:
-            splitter = shlex.shlex(command.encode("utf-8"))
-        splitter.whitespace = "|"
-        splitter.whitespace_split = True
-        command = []
-
-        while True:
-            token = splitter.get_token()
-            if token:
-                command.append(token)
-            else:
-                break
-
-        command = list(map(shlex.split, command))
-
-    return command
-
-
-def chain(command, timeout=TIMEOUT, cwd=None, env=None):
-    commands = _expand_args(command)
-    data = None
-
-    for command in commands:
-
-        c = run(command, block=False, timeout=timeout, cwd=cwd, env=env)
-
-        if data:
-            c.send(data)
-            c.subprocess.sendeof()
-
-        data = c.out
-
-    return c
